98a838934afd9cc1209bbff64f9be1899f5da108
Hossanadev <hossanacc@gmail.com>
2023-04-22 01:22:27 +0100

    Addition of Layout

---
+++ components/Layout.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+
+const Layout = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + "store" : "null"}</title>
+        <meta name="description" content="ecommerce store"></meta>
+        <link rel="icon" href=""></link>
+      </Head>
+
+      <div className="flex flex-col justify-between min-h-screen">
+        <header>
+          <nav className="border-b shadow-lg border-green-800 flex justify-between items-center h-12 px-4">
+            <Link className="text-xl" href={"/"}>
+              Hossanastore
+            </Link>
+            <div className="flex gap-4">
+              <Link href={"/cart"}>Cart</Link>
+              <Link href={"/login"}>Login</Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto ml-4 mt-4">{children}</main>
+        <footer className="flex justify-center items-center h-12 shadow-inner border-t border-green-800">
+          Copyright Hossanastore Worldwide 2023
+        </footer>
+      </div>
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Layout from "@/components/Layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-3xl font-medium">Hossanastore</div>
-    </main>
-  );
+  return <Layout title={"Hossana"}>content</Layout>;
 }
