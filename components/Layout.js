893607a321fc89a571431244bfafc107be85a46e
Hossanadev <hossanacc@gmail.com>
2023-04-27 11:48:33 +0100

    addition: sidebar drawer

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import Head from "next/head";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { IoAppsSharp } from "react-icons/io5";
+import Sidebar from "./Sidebar";
 
 const Layout = ({ title, children }) => {
+  const [openSideBar, setOpenSiseBar] = useState(false);
+  const handleSideBar = () => {
+    setOpenSiseBar(!openSideBar);
+  };
   return (
     <>
       {/* head */}
@@ -14,14 +19,14 @@ const Layout = ({ title, children }) => {
       </Head>
 
       {/* header */}
-      <div className="flex flex-col justify-between min-h-screen">
-        <header>
-          <nav className="shadow-md border-primary_color flex justify-between items-center h-16 md:px-4 px-2">
+      <div className="overflow-x-hidden flex flex-col justify-between min-h-screen">
+        <header className="fixed w-full z-30 top-0">
+          <nav className="shadow-md w-screen border-primary_color z-30 bg-white flex justify-between items-center h-16 md:px-8 px-2">
             <Link
               className="hover:translate-x-4 transition-all duration-700"
               href={"/"}
             >
-              <span className="text-xl px-1 text-black_color border font-medium md:font-semibold border-secondary_color  ml-1 rounded-r-full border-r-0">
+              <span className="text-xl text-black_color border font-medium md:font-semibold px-1 border-secondary_color rounded-r-full border-r-0">
                 Hossana
               </span>
               <span className="px-2 md:font-medium font-medium py-0.5 text-lg rounded-tr-2xl rounded-bl-md border bg-primary_color/90 text-white border-yellow-600 border-r-0 border-l-0">
@@ -42,6 +47,7 @@ const Layout = ({ title, children }) => {
                 Cart
               </Link>
               <IoAppsSharp
+                onClick={handleSideBar}
                 size={30}
                 className="cursor-pointer text-primary_color hover:text-secondary_color duration-700 transition-all"
               />
@@ -49,13 +55,22 @@ const Layout = ({ title, children }) => {
           </nav>
         </header>
 
+        {/* side nav */}
+        <aside
+          className={`ease-in-out duration-1000 sidebar z-10 ${
+            openSideBar ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          <Sidebar className="bg-[#000000]" />
+        </aside>
+
         {/* main */}
-        <main className="container m-auto mt-4 py-1 mb-4 px-2 md:py-16">
+        <main className="container m-auto mt-16 py-1 mb-4 px-2 md:py-16">
           {children}
         </main>
 
         {/* footer */}
-        <footer className="flex justify-center items-center h-28 shadow-inner border-green-800">
+        <footer className="flex justify-center z-30 bg-white items-center h-28 shadow-inner border-green-800">
           Copyright Hossana-stores 2023
         </footer>
       </div>
---
+++ components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <>
+      <div className="p-2 px-8 mt-[4rem]">Sidebar Details</div>
+    </>
+  );
+};
+
+export default Sidebar;
